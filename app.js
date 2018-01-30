789a65b1de128f50f7f3db7b0876b77496d24d86
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-30 12:50:08 -0500

    Get Frequencies JS function

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write your code here!
+function getFrequencies(str) {
+  const sorted = str.split('').sort();
+  const data = [];
+  for (let i = 0; i < sorted.length; i++) {
+    const last = data[data.length -1];
+    if (last && last.character === sorted[i]) last.count++; //preventing duplicates
+    else data.push({ character: sorted[i], count: 1}); //adds to array of objects of letters with count
+  }
+  return data;
+}
